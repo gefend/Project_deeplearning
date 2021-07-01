d55482dfd90961dc07f9c50b1ab17c7e75aad1a5
gefend <83830259+gefend@users.noreply.github.com>
2021-07-01 09:08:01 +0300

    Add files via upload

--- create_test_csv.py
+++ create_test_csv.py
@@ -11,11 +11,15 @@ train_samples, val_samples, test_samples = get_train_val_samples(VAL_KEY_ID_PATH
 test_df = pd.read_csv(config.TEST_SIMPLIFIED_PATH)
 
 data=[]
+data_compare=[]
 n_samples=len(test_samples[0])
 key_id=np.arange(n_samples)
 for i in range(n_samples):
     point={"key_id":key_id[i],"countrycode":test_samples[2][i],"drawing":test_samples[0][i]}
+    point_compare={"key_id":key_id[i],"countrycode":test_samples[2][i],"drawing":test_samples[0][i],"word":test_samples[1][i]}
     data.append(point)
+    data_compare.append(point_compare)
 df = pd.DataFrame(data)
+df_compare=pd.DataFrame(data_compare)
 df.to_csv('./data/my_test_simplified.csv', index=False)
-
+df_compare.to_csv('./data/my_test_simplified_compare.csv', index=False)
