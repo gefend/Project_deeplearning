e2f2584c86df64dc70580c7daa16d43e71b774fb
gefend <83830259+gefend@users.noreply.github.com>
2021-07-01 09:05:16 +0300

    Add files via upload

---
+++ create_test_csv.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+
+from src.datasets import DrawDataset, get_train_val_samples
+from src import config
+
+VAL_KEY_ID_PATH = './data/val_key_ids_002.json'
+TRAIN_KEY_ID_PATH = './data/train_key_ids_002.json'
+TEST_KEY_ID_PATH = './data/test_key_ids_002.json'
+train_samples, val_samples, test_samples = get_train_val_samples(VAL_KEY_ID_PATH, TRAIN_KEY_ID_PATH, TEST_KEY_ID_PATH)
+test_df = pd.read_csv(config.TEST_SIMPLIFIED_PATH)
+
+data=[]
+n_samples=len(test_samples[0])
+key_id=np.arange(n_samples)
+for i in range(n_samples):
+    point={"key_id":key_id[i],"countrycode":test_samples[2][i],"drawing":test_samples[0][i]}
+    data.append(point)
+df = pd.DataFrame(data)
+df.to_csv('./data/my_test_simplified.csv', index=False)
+
